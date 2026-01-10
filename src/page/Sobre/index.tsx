4e22d6502b1efb57ddf16cdbaaebaebe9a3adb24
Alício Mussenga <aliciomussenga@gmail.com>
2026-01-10 14:54:10 +0100

    Adicionando os componentes

--- src/page/Sobre/index.tsx
+++ src/page/Sobre/index.tsx
@@ -1,9 +1,15 @@
-
+import Manifesto from "../../sections/Manifesto";
+import Pilares from "../../sections/Pilares";
 
 function Sobre() {
   return (
-    <div>Sobre</div>
-  )
+    <main className="bg-gson-black min-h-screen px-[7%] pt-32">
+      <div className="max-w-7xl mx-auto">
+        <Manifesto />
+        <Pilares />
+      </div>
+    </main>
+  );
 }
 
-export default Sobre
+export default Sobre;
